64b5fc1045d96685f4e246f9ed847f7433edec25
prajyot bhopale <prajyot1818@gmail.com>
2025-10-06 19:35:34 +0530

    add functionality

--- 10_todoContextLocalStorage/src/App.jsx
+++ 10_todoContextLocalStorage/src/App.jsx
@@ -2,15 +2,31 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [todos, setTodos] = ([])
 
+ const addTodo = (todo) =>{
+  setTodos((prev) => [ {id: Date.now(), ...todo}, ...prev])
+ }
+
+ const updateTodo = (id, todo) =>{
+  setTodos((prev) => prev.map((prevTodo) => (prevTodo.id ===todo.id ? todo : prevTodo)))
+ }
+
+ const deleteTodo = (id) =>{
+  setTodos((prev) => prev.filter((todo) => todo.id !== id))
+ }
+
+ const toggleComplete = (id) =>{
+  setTodos((prev) => prev.map((todo) => (todo.id === id ? {...todo, completed: !todo.completed} : todo)))
+ }
   return (
-    <>
+    <TodoProvider values={{todos, addTodo,updateTodo,deleteTodo, toggleComplete}}>
    <h1 className="text-3xl font-bold underline bg-red-500">hello world</h1>
      
-    </>
+    </TodoProvider>
   )
 }
 
