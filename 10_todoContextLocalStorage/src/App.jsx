f21a399e0d07501edc6b3accf3338964cb95323e
prajyot bhopale <prajyot1818@gmail.com>
2025-10-07 17:07:26 +0530

    completed the reaming part of this project

--- 10_todoContextLocalStorage/src/App.jsx
+++ 10_todoContextLocalStorage/src/App.jsx
@@ -1,33 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { TodoProvider } from './contexts'
+
+import { TodoProvider } from "./contexts";
+import { useState,useEffect } from "react";
+import {TodoForm, TodoItem} from "./components"
 
 function App() {
- const [todos, setTodos] = ([])
+  const [todos, setTodos] = useState([]);
 
- const addTodo = (todo) =>{
-  setTodos((prev) => [ {id: Date.now(), ...todo}, ...prev])
- }
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
 
- const updateTodo = (id, todo) =>{
-  setTodos((prev) => prev.map((prevTodo) => (prevTodo.id ===todo.id ? todo : prevTodo)))
- }
+  const updateTodo = (id, todo) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo.id === todo.id ? todo : prevTodo))
+    );
+  };
 
- const deleteTodo = (id) =>{
-  setTodos((prev) => prev.filter((todo) => todo.id !== id))
- }
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
- const toggleComplete = (id) =>{
-  setTodos((prev) => prev.map((todo) => (todo.id === id ? {...todo, completed: !todo.completed} : todo)))
- }
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo.id === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos && todos.length > 0) {
+      setTodos(todos);
+    }
+  }, []);
+
+  useEffect(() =>{
+    localStorage.setItem ("todos", JSON.stringify(todos));
+  }), [todos];
   return (
-    <TodoProvider values={{todos, addTodo,updateTodo,deleteTodo, toggleComplete}}>
-   <h1 className="text-3xl font-bold underline bg-red-500">hello world</h1>
-     
+     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                        <TodoForm />
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'
+                          >
+                            <TodoItem todo={todo} />
+                          </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
     </TodoProvider>
-  )
+  );
 }
 
-export default App
+export default App;
